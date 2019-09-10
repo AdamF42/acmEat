8d9ebf140903e7305ffc04764bcf2cffa8ecdf73
Adamo Fapohunda <adamofapohunda@gmail.com>
2019-09-10 09:46:30 +0200

    Updated postman collection

--- acme-agency/common/src/main/java/it/unibo/utils/Services.java
+++ acme-agency/common/src/main/java/it/unibo/utils/Services.java
@@ -3,10 +3,10 @@ package it.unibo.utils;
 
 public class Services {
 
-    public static final String DELIVERY_COMPANY_ONE_URL="http://localhost:10002/delivery/";
-    public static final String DELIVERY_COMPANY_TWO_URL="http://localhost:10003/delivery/";
-    public static final String GIS_SERVICE_URL="http://localhost:10004/";
-    public static final String BANK_REST_SERVICE_URL ="http://localhost:10001/bank/";
+    public static final String DELIVERY_COMPANY_ONE_URL="http://192.168.99.100:10002/delivery/";
+    public static final String DELIVERY_COMPANY_TWO_URL="http://192.168.99.100:10003/delivery/";
+    public static final String GIS_SERVICE_URL="http://192.168.99.100:10004/";
+    public static final String BANK_REST_SERVICE_URL ="http://192.168.99.100:10001/bank/";
 
 }
 
