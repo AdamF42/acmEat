6993fa1228cc69a9197155af198a3df226a161ca
Adamo Fapohunda <adamofapohunda@gmail.com>
2019-09-17 21:32:03 +0200

    updated delivery service

--- deliveryService/src/main/java/it/unibo/models/Order.java
+++ deliveryService/src/main/java/it/unibo/models/Order.java
@@ -5,19 +5,19 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
 public class Order {
+    public Integer id;
+    public Double price;
     public String company;
     public String src_address;
     public String dest_address;
     public String delivery_time;
-    public Integer id;
     public Status status;
-    public Double price;
 
-    public static Status checkAndSetStatus(Status status){
-        if(status == Status.AVAILABLE){
-            return Status.ACCEPTED;
-        }else{
-            return Status.NOT_ACCEPTED;
+    public void checkAndSetAcceptedStatus() {
+        if (status == Status.AVAILABLE) {
+            status = Status.ACCEPTED;
+        } else {
+            status = Status.NOT_ACCEPTED;
         }
     }
 }
